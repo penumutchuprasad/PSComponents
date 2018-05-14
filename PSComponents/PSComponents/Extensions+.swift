1f1086b5423e6c3c7817b2ff6237f13669514745
penumutchu.prasad@gmail.com <penumutchu.prasadgmail.com@penumutchu-MacBook-Pro.local>
2018-05-14 19:58:29 +0530

    just framework folder is added

---
+++ PSComponents/PSComponents/Extensions+.swift
@@ -0,0 +1,43 @@
+//
+//  Extensions+.swift
+//  PSComponents
+//
+//  Created by penumutchu.prasad@gmail.com on 14/05/18.
+//  Copyright © 2018 abnboys. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    
+    public func presentAlert(withTitle title: String, message: String, actionTitles: [String: UIAlertActionStyle], completion: ((_ title: String)->(Any?))? = buttonAction ) {
+        
+        let alertVC = UIAlertController.init(title: title, message: message, preferredStyle: .alert)
+        
+        //        let actnTitles = actionTitles.reversed()
+        
+        for (index,action) in actionTitles.enumerated() {
+            
+            alertVC.addAction(UIAlertAction.init(title: action.key, style: action.value, handler: { (_) in
+                
+                _ = completion!(action.key)
+                
+            }))
+            
+        }
+        
+        defer {
+            present(alertVC, animated: true, completion: nil)
+        }
+        
+    }
+    
+    
+    public  class func buttonAction (title: String)->(Any?) {
+        
+        print("Extension Action is called")
+        return nil
+    }
+    
+}
---
+++ PSComponents/PSComponents/PSComponents.h
@@ -0,0 +1,19 @@
+//
+//  PSComponents.h
+//  PSComponents
+//
+//  Created by penumutchu.prasad@gmail.com on 14/05/18.
+//  Copyright © 2018 abnboys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PSComponents.
+FOUNDATION_EXPORT double PSComponentsVersionNumber;
+
+//! Project version string for PSComponents.
+FOUNDATION_EXPORT const unsigned char PSComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSComponents/PublicHeader.h>
+
+
